ea98bfd8c56d94b2d4d6dda29443d64de3452e9c
Clite Tailor <ducnhatphung@gmail.com>
2018-09-23 23:27:40 +0700

    First completion on documentation; Add fallback version of lift operator;

--- src/lift.js
+++ src/lift.js
@@ -6,15 +6,27 @@ import { switchMap, map } from 'rxjs/operators'
  *
  * ```js
  *
- *    lift(state$, action$, reducer)
+ *    lift(state$, action$, operator)
  *      .subscribe(state$)
  *
  * ```
  */
-export function lift(...args, func) {
-  if (args.length === 2) {
-    return lift(func)(...args)
+export function lift(state$, action$, func) {
+  if (!func) {
+    /**
+     * Include fallback to the real semantic version of lift:
+     *
+     * ```js
+     *
+     *    lift(operator)(state$, action$)
+     *      .subscribe(state$)
+     *
+     * ```
+     */
+    func = state$
+    return (state$, action$) => lift(state$, action$, func)
   }
+
   return state$.pipe(
     switchMap(state =>
       action$.pipe(map(action => func(state, action)))
